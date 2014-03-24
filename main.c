46d8bfb182f11da0c96277fa618da1ecc03a3a73
Lee jongwoo <whddn0209@gmail.com>
2014-03-24 21:09:01 +0900

    add main.c in the repository

---
+++ main.c
@@ -0,0 +1,25 @@
+// main.c //
+
+#include <stdio.h>
+int main()
+{
+	int A[10];
+	int n=10;
+	int i;
+
+	printf("정렬하고 싶은 숫자 10개를 입력\n");
+	
+	for (i=0; i<n; i++) // 10개의 숫자 입력받기
+	{
+		printf("%d : ", i+1);
+		scanf("%d", &A[i]);
+	}
+
+	quick_sort(A, n); // quick_sort 함수 호출
+
+	for(i=0; i<n; i++) // 정렬된 숫자 출력하기
+		printf("%d ", A[i]);
+		printf("\n\n");
+
+		return 0;
+}
