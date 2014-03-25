b9615c66c6114209dee86d99bd992fc985e943a2
Lee jongwoo <whddn0209@gmail.com>
2014-03-25 11:34:48 +0900

    quick_sort2.c has changed

--- quick_sort2.c
+++ quick_sort2.c
@@ -1,41 +1,42 @@
 // quick_sort.c	       //
-// quick sort function //
+// quick sort modified function //
 
 void quick_sort(int A[], int n)  // quick sort 구현
 {
-	int pivot, bin;
-	int left_1, right_1_1;
+	int pivot_changed, temp_changed;// change element
+	int left_changed, right_changed;// change element
 	int i;
 
 	if(n > 1) // 분할 크기가 1보다 클때까지
 	{
-		pivot = A[n-1]; //맨 우측 값을 pivot으로
-		left_1 = -1;
-		right_1_1 = n-1;
+		pivot_changed = A[n-1]; //맨 우측 값을 pivot_changed으로
+		left_changed = -1;
+		right_changed = n-1;
 		
-		whileft_1e(1)
+		while(1)
 		{
-			whileft_1e(A[++left_1] < pivot); 
-				whileft_1e(A[--right_1_1] > pivot); 
-					if(left_1 >= right_1_1)
-					// left_1가 right_1_1보다 같거나 클 경우 분할 중단
+			while(A[++left_changed] < pivot_changed); 
+				while(A[--right_changed] > pivot_changed); 
+					if(left_changed >= right_changed)
+					// left_changed가 right_changed보다 같거나 클 경우 분할 중단
 						break; 
 
-					eleft_1se
+					else
 					{	
-						// right_1_1가 left_1보다 클 경우 left_1값과 right_1_1값을 변경
-						bin = A[left_1];
-						A[left_1] = A[right_1_1];
-						A[right_1_1] = bin;
+						// right_changed가 left_changed보다 클 경우 left_changed값과 right_changed값을 변경
+						temp_changed = A[left_changed];
+						A[left_changed] = A[right_changed];
+						A[right_changed] = temp_changed;
 					}
+
 		}
 		
-		// pivot값과 pivot값의 위치에 있는 값을 바꿈
-		bin = A[left_1];
-		A[left_1] = A[n-1];
-		A[n-1] = bin;
+		// pivot_changed값과 pivot_changed값의 위치에 있는 값을 바꿈
+		temp_changed = A[left_changed];
+		A[left_changed] = A[n-1];
+		A[n-1] = temp_changed;
 
-		quick_sort(A, left_1); // left_1 정렬
-		quick_sort(A+left_1+1, n-left_1-1); // right_1_1 정렬
+		quick_sort(A, left_changed); // left_changed 정렬
+		quick_sort(A+left_changed+1, n-left_changed-1); // right_changed 정렬
 	}
 }
